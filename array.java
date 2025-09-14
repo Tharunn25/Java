115937b5e1724a6ca16dace271a1a14ab0439029
who is Tharunn <98308855+Tharunn25@users.noreply.github.com>
2025-09-14 23:16:35 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,11 @@
+public class array
+{
+    public static void main(String a[])
+    {
+        int nums[] = {4,6,426,25,23};
+
+        for(int i=0;i<nums.length;i++)
+            System.out.println(nums[i]);
+        
+    }
+}
