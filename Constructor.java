e053500887309dd8d80564902dc1b2a6937f9105
who is Tharunn <98308855+Tharunn25@users.noreply.github.com>
2025-10-27 22:46:45 +0530

    Add files via upload

---
+++ Constructor.java
@@ -0,0 +1,9 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+public class Constructor {
+    public Constructor() {
+    }
+
+    public static void main(String[] var0) {
+        System.out.println("hello world");
+    }
+}
