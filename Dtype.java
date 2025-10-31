c7b9793e275b759e66defd0ba44aa3625bb2c1e2
who is Tharunn <98308855+Tharunn25@users.noreply.github.com>
2025-10-31 23:56:45 +0530

    Add files via upload

---
+++ Dtype.java
@@ -0,0 +1,20 @@
+public class Dtype{
+    public static void main(String[] args){
+        int age = 21;
+        String name = "Leo das";
+        char letter = 'D';
+        boolean isFun = true;
+        float d = 9.2f;
+        double weight = 63.5;
+
+        System.out.println("age is " + age);
+        System.out.println("name is " + name);
+        System.out.println("Letter is " + letter);
+        System.out.println("is it fun  " + isFun);
+        System.out.println("the float is " + d);
+        System.out.println("The weight is " + weight);
+
+
+
+    }
+}
