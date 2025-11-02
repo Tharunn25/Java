6f66c0525b395a67217d625a6af6003af0e822be
who is Tharunn <98308855+Tharunn25@users.noreply.github.com>
2025-11-02 21:55:52 +0530

    Add files via upload

--- SimpleInterestCalculator.java
+++ SimpleInterestCalculator.java
@@ -3,7 +3,34 @@ import java.util.Scanner;
 
 public class SimpleInterestCalculator {
     public static void main(String[] args) {
-        
+
+        Scanner ns = new Scanner(System.in);
+        String choice;
+
+        do{
+              System.out.println("Enter the principal amount : ");
+        double principalAmount = ns.nextDouble();
+
+        System.out.println("Enter the Interest rate : ");
+        double interestRate = ns.nextDouble();
+
+        System.out.println("Enter the time duration of the load (In years) : ");
+        double time = ns.nextDouble();
+
+        double simpleInterest = (principalAmount * interestRate * time)/100.00;
+        double totalAmount = principalAmount + simpleInterest;
+
+        System.out.printf("Simple Interest is %.2f%n",simpleInterest);
+        System.out.printf("Total Amount is %.2f%n", totalAmount);
+
+        ns.nextLine();
+
+        System.out.println("Do you want to perform the Interest calculation again : ");
+        choice = ns.nextLine();
+        }
+        while(choice.equalsIgnoreCase("yes"));
+        System.out.println("Thank you for using Interest Calculator");
+        ns.close();  
     }
     
 }
