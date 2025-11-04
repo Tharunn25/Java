70cdea81b32b2048c6e4a0c38a44dbbd5ce7f9b7
who is Tharunn <98308855+Tharunn25@users.noreply.github.com>
2025-11-04 22:21:10 +0530

    Add files via upload

--- AtmMachine.java
+++ AtmMachine.java
@@ -8,17 +8,58 @@ public class AtmMachine {
 
         int pin = 1234;
 
-        System.out.println("Please Enter Your ATM Pin : ");
-        int userPin = ns.nextInt();
+        double balance = 1000.00;
 
-        if(userPin == pin){
-            System.out.println("Access Granted ✅");
-        }
-        else{
-            System.out.println("Please Enter a correct Pin");
-        }
+        System.out.println("Please Enter your pin : ");
+        int enteredPin = ns.nextInt();
 
+        if(enteredPin != pin){
+            System.out.println("Invalid Pin access denied");
+            return;
+        }
+        System.out.println("Access Granted");
+
+        int choice;
         
-    }
-    
+        do{
+            System.out.println("\n----- ATM Menu -----");
+            System.out.println("1. Check Balance");
+            System.out.println("2. Deposit Money");
+            System.out.println("3. Withdraw Money");
+            System.out.println("4. Exit");
+            choice = ns.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.printf("Your Current Balance is $%.2f%n",balance);
+                    break;
+                
+                case 2:
+                    System.out.println("Enter the money to deposit : ");
+                    double deposit = ns.nextDouble();
+                    balance += deposit;
+                    System.out.printf("Your money has been deposited successfully, your new balance is $%.2f%n",balance);
+                    break;
+                case 3: 
+                    System.out.println("Enter the amount to withdraw : ");
+                    double withdraw = ns.nextDouble();
+                    if(withdraw > balance){
+                        System.out.println("Insufficient Balance");
+                    }
+                    else{
+                        balance -= withdraw;
+                        System.out.printf("Withdrawal successful, Your current balance is %.2f%n",balance);
+                    }
+                    break;
+                case 4:
+                    System.out.println("Thank you for using the ATM. Goodbye !");
+                    break;
+                
+                default:
+                    System.out.println("Invalid option please try again.");
+                    break;
+            }
+            }while(choice!=4);
+            ns.close();
+        }    
 }
