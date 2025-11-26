e24ec49f9bfd5f19f571bb217a3ee14c7435bbf5
who is Tharunn <98308855+Tharunn25@users.noreply.github.com>
2025-11-26 23:12:41 +0530

    Add files via upload

---
+++ Dog.java
@@ -0,0 +1,14 @@
+
+package animals;
+
+public class Dog {
+        public String name = "Bruno";
+        protected int age = 4;
+        String type = "labrador";
+        private int price = 20000;
+
+        public void display(){
+            System.out.println("Dog name: " + name);
+        }
+
+}
