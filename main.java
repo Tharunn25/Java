e33b93256f602f6d6f7a9a95be4fc93dcda9a719
who is Tharunn <98308855+Tharunn25@users.noreply.github.com>
2025-11-26 23:12:58 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,14 @@
+
+package mainapp;
+import animals.*;
+
+public class main {
+
+    public static void main(String[] args) {
+        Dog d1 = new Dog();
+        System.out.println(d1.name);
+        d1.display();
+    }
+
+    
+}
